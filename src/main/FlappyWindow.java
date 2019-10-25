cc80f1db96b2395fa3878456c680834bfe3fbe46
D3PSI <cedricschwyter@bluewin.ch>
2019-10-25 09:17:41 +0200

    Update comments

--- src/main/FlappyWindow.java
+++ src/main/FlappyWindow.java
@@ -3,7 +3,7 @@ package main;
 public class FlappyWindow {
 
 	/**
-	 * 
+	 * This method defines the entry point for the application
 	 * @param args
 	 */
 	public static void main(String[] args) {
