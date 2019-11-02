15d35c1a39d6df53c9de5b6181ded73543b70965
D3PSI <d3psigames@gmail.com>
2019-11-02 16:31:42 +0100

    Convert to Gradle build, update .travis.yml

---
+++ src/main/java/com/flappyai/buffers/VertexBuffer.java
@@ -0,0 +1,19 @@
+package com.flappyai.buffers;
+
+/**
+ * Defines a handler for a vertex buffer
+ * @author d3psi
+ */
+public class VertexBuffer {
+
+	public int VBO = 0;
+	private float[] vertices;
+	
+	/**
+	 * Constructor with vertex data
+	 */
+	public VertexBuffer(float[] vertices_) {
+		this.vertices = vertices_;
+	}
+	
+}
