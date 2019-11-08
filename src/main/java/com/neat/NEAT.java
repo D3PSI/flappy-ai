6b3920976fd5e7bb4cfcad4e1dfdf5ab89571e33
Pluscrafter <pluscrafter123@hotmail.com>
2019-11-08 15:07:19 +0100

    add jenetics test

--- src/main/java/com/neat/NEAT.java
+++ src/main/java/com/neat/NEAT.java
@@ -1,9 +1,22 @@
 package com.neat;
 
+import io.jenetics.BitChromosome;
+import io.jenetics.BitGene;
+import io.jenetics.Genotype;
+import io.jenetics.engine.Engine;
+import io.jenetics.engine.EvolutionResult;
+import io.jenetics.util.Factory;
+
 /**
  * Main class for neuroevolutic genetic algorithm
  * @author d3psi
  */
 public class NEAT {
+	
+    public static int eval(Genotype<BitGene> gt) {
+        return gt.getChromosome()
+            .as(BitChromosome.class)
+            .bitCount();
+    }
 
 }
