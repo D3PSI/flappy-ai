ebd38c884915673758bb35d3a49742794edd0ef8
D3PSI <cedricschwyter@bluewin.ch>
2019-11-29 08:51:16 +0100

    Fix

--- src/main/java/com/models/Bird.java
+++ src/main/java/com/models/Bird.java
@@ -43,8 +43,10 @@ public class Bird extends Model2D {
         for(int i = 0; i < 4; i++) {
             yOff += yVel * FlappyWindow.deltaTime;
             yVel += -0.5f * FlappyWindow.deltaTime; 
-            if (yOff < -0.8f)
+            if (yOff < -0.8f) {
                 yOff = -0.8f;
+                yVel = 0.0f;
+            }
             float u = vertices[i][0] + xOff;
             float v = vertices[i][1] + yOff;
             float x = texCoords[i][0];
@@ -59,7 +61,7 @@ public class Bird extends Model2D {
      * Makes the bird jump
      */
     public void jump() {
-        yVel -= 3.0;
+        yVel = -1.0f;
     }
 
 }
